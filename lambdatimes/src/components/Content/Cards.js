0f862f9a89e73465e874df70217a5b42270d113f
BenR <thisbenrogers@gmail.com>
2019-06-07 13:23:16 -0500

    display all cards, tabbing pagination works

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import Card from './Card';
+
+const CardContainer = styled.div``;
 
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <CardContainer className="cards-container">
+      {props.cards.map((card, index) => {
+        return <Card card={card} key={index} />
+      })}
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
-    </div>
+    </CardContainer>
   )
 }
 
